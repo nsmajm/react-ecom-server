1ac6f9b9d4c63f3d71613c1149eeaf58acf70eea
nsamjm <nsmajm@gmail.com>
2019-08-29 23:42:13 +0600

    added user all in route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/users',function(){
+    return User::all();
+})
